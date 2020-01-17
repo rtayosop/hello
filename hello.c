f64b71db5f61a098c336cd174df242a29d895e4d
Phaniso87 <59966296+Phaniso87@users.noreply.github.com>
2020-01-17 11:39:15 -0600

    Added my name and major

--- hello.c
+++ hello.c
@@ -6,5 +6,7 @@ Date: 2020/01/14
 int main(void) {
     printf("Hello World!\n");
     Printf("My major: Computer Engineering\n");
+    printf("Name: Phaniso Shaba\n");
+    printf("Major: Electrical Engineering\n");
     return 0;
 }
